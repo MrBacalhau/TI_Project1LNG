85eaf4b1f00b4f3c82801e9a1eeec97ae5c0ce25
Lucas Silva <lukaniaco@hotmail.com>
2018-09-25 19:26:28 +0100

    update main

--- Main.m
+++ Main.m
@@ -1,13 +1,16 @@
-[filepath,name,ext] = fileparts('english.txt');
+file='dados\song01.wav';
+[filepath,name,ext] = fileparts(file);
 
 if(ext == '.txt')
-    fid = fopen(filepath,'r');
-    final = fscanf(fid);
-   % visualiza(final)
-    drawhist();
+    id = fopen(file);
+    inf = fread(id);
 elseif(ext == '.wav')
-    aid= audioread(filepath);
-    ainf= audioinfo(filepath);
-    drawhist();
-  
+    [id, fs]= audioread(file);
+    inf= audioinfo(file);
+elseif(ext== '.bmp')
+   % id=
+    %inf=
 end
+    
+    %hist=drawhist(id, inf);
+    %disp('Entropia de %s: %f\n', name, entropia(hist));
