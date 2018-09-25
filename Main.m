407529c9cdd97f169229239b8438f8f3ea2c5cca
Nuno Caridade <nmcaridade@gmail.com>
2018-09-25 19:30:37 +0100

    troll

--- Main.m
+++ Main.m
@@ -1,13 +1,1 @@
-[filepath,name,ext] = fileparts('english.txt');
-
-if(ext == '.txt')
-    fid = fopen(filepath,'r');
-    final = fscanf(fid);
-   % visualiza(final)
-    drawhist();
-elseif(ext == '.wav')
-    aid= audioread(filepath);
-    ainf= audioinfo(filepath);
-    drawhist();
-  
-end
+fprintf('ola')
