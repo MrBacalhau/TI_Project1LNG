7f250974cb69c7377286c4713b9142823bbcf8dd
Nuno Caridade <nmcaridade@gmail.com>
2018-09-25 19:36:23 +0100

    Revert "troll"
    This reverts commit 407529c9cdd97f169229239b8438f8f3ea2c5cca.

--- Main.m
+++ Main.m
@@ -1,1 +1,13 @@
-fprintf('ola')
+[filepath,name,ext] = fileparts('english.txt');
+
+if(ext == '.txt')
+    fid = fopen(filepath,'r');
+    final = fscanf(fid);
+   % visualiza(final)
+    drawhist();
+elseif(ext == '.wav')
+    aid= audioread(filepath);
+    ainf= audioinfo(filepath);
+    drawhist();
+  
+end
