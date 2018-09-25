616881bead643418126659495e4a103a552bbd7c
Lucas Silva <lukaniaco@hotmail.com>
2018-09-25 19:46:20 +0100

    new drawhist

--- Main.m
+++ Main.m
@@ -4,16 +4,16 @@ file='dados\english.txt';
 
 if(ext == '.txt')
     id = fopen(file);
-    inf = fread(id);
+    Y = fread(id);
     fclose(id);
 elseif(ext == '.wav')
-    [id, fs]= audioread(file);
+    [Y, fs]= audioread(file);
     inf= audioinfo(file);
 elseif(ext== '.bmp')
-    id=imageread(file);
+    Y=imageread(file);
     inf=imageinfo(file);
 end
     
-    %hist=drawhist(id, inf);
-    %disp('Entropia de %s: %f\n', name, entropia(hist));
+    hist=drawhist(Y);
+    disp('Entropia de %s: %f\n', name, entropia(hist));
 
