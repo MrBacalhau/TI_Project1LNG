6d4191e157c62497d901fdb566428cbc14f5d7de
Nuno Caridade <nmcaridade@gmail.com>
2018-09-25 19:34:29 +0100

    troll

--- Main.m
+++ Main.m
@@ -1,1 +1,16 @@
-fprintf('ola')
+file='dados\song01.wav';
+[filepath,name,ext] = fileparts(file);
+
+if(ext == '.txt')
+    id = fopen(file);
+    inf = fread(id);
+elseif(ext == '.wav')
+    [id, fs]= audioread(file);
+    inf= audioinfo(file);
+elseif(ext== '.bmp')
+   % id=
+    %inf=
+end
+    
+    %hist=drawhist(id, inf);
+    %disp('Entropia de %s: %f\n', name, entropia(hist));
