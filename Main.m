44f611450ac6665cd44cc07e36ea0f487146db8f
Gonçalo Ferreira <gonff@users.noreply.github.com>
2018-10-16 18:26:00 +0100

    n

--- Main.m
+++ Main.m
@@ -1,4 +1,4 @@
-file='dados\english.txt';
+file='dados\homer.bmp';
 [filepath,name,ext] = fileparts(file);
 
 if(ext == '.txt')
@@ -14,5 +14,8 @@ elseif(ext== '.bmp')
 end
     
     hist=drawhist(Y);
-    entropia(Y);
+    entropia(hist);
+    HL=hufflen(hist);
+    
+    infomutua([2 6 4 10 5 9 5 8 0 8], [6 8 9 7 2 4 9 9 4 9 1 4 8 0 1 2 2 6 3 2 0 7 4 9 5 4 8 5 2 7 8 0 7 4 8 5 7 4 3 2 2 7 3 5 2 7 4 9 9 6],0 : 10,1);
 
