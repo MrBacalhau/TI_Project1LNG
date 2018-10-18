d4929f9331b3d68eca26abcefb2e100f3082fd6a
Lucas Silva <lukaniaco@hotmail.com>
2018-10-18 19:53:50 +0100

    jsdjfj

--- shazam.m
+++ shazam.m
@@ -8,16 +8,18 @@ function [  ] = shazam( Y, Yquery, passo, alphabeto )
         Psearch=histsearch/sum(histsearch);
         PaPb=Pquery.*Psearch;
         PaPb(PaPb == 0) = 1;
-        eqCount=zeros(1,length(alphabeto));
+        eqCount=zeros(length(alphabeto),length(alphabeto));
         for j=1:length(Yquery)
-            if(Ysearch(j)==Yquery(j))
-                a=find(alphabeto==Y(j));
-                eqCount(a)=eqCount(a)+1;
-            end
+            %if(Ysearch(j)==Yquery(j))
+            a=find(alphabeto==Ysearch(j));
+            b=find(alphabeto==Yquery(j));
+            eqCount(a,b)=eqCount(a,b)+1;
+            %end
         end
-        eqCount(eqCount==0)=1;
-        probconj=eqCount/(sum(eqCount));
-        infmut(end+1)=sum(probconj.*log2(probconj./PaPb));
+        probconj=eqCount/sum(sum(eqCount));
+        probconj=nonzeros(probconj);
+        infmut(end+1)=sum(Pquery)+sum(Psearch)-sum(probconj);
+        %infmut(end+1)=probconj.*log2(probconj./PaPb);
     end
     disp (infmut);
 
